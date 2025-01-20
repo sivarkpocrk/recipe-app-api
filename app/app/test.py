55e970c0151ddcf16ed4f91000eec2294f4a9bf2
sivak1106 <92651216+sivarkpocrk@users.noreply.github.com>
2025-01-20 00:25:43 +0000

    Fix PEP 8 style violations

--- app/app/test.py
+++ app/app/test.py
@@ -5,6 +5,7 @@ from app import calc
 class CalcTest(SimpleTestCase):
 
     def test_add_num(self):
-        res = calc.add(5, 3)
-        self.assertEqual(res, 8)
         
+        res = calc.add(5, 3)
+
+        self.assertEqual(res, 8)
