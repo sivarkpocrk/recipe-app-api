1991b7dad55cb7fd56d95e2e0a3e0c0aa8040071
sivak1106 <92651216+sivarkpocrk@users.noreply.github.com>
2025-01-20 00:15:22 +0000

    Added GitHub Actions - added unit test

--- app/app/test.py
+++ app/app/test.py
@@ -1,9 +1,11 @@
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTest(SimpleTestCase):
-  
-  def test_add_num(self):
-      res = calc.add(5, 3)
-      self.assertEqual(res, 8)
+
+    def test_add_num(self):
+        res = calc.add(5, 3)
+        self.assertEqual(res, 8)
+
 
