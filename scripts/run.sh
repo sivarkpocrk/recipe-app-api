611ecb2c6395a1d356e479f5288e7cb919e8de66
sivak1106 <92651216+sivarkpocrk@users.noreply.github.com>
2025-03-22 15:01:00 +0000

    fixing nginx permission issue

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+# ✅ Fix permissions
+chown -R django-user:django-user /vol/web/static /vol/web/media
+chmod -R 755 /vol/web/static /vol/web/media
+
+
 python manage.py wait_for_db
 python manage.py collectstatic --noinput
 python manage.py migrate
