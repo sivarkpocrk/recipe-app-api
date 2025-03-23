e96f4f1d471572a53ba19e6962bfa8890ee40408
sivak1106 <92651216+sivarkpocrk@users.noreply.github.com>
2025-03-23 23:39:22 +0000

    added terraform

---
+++ infra/setup/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.23.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "devops-siva-recp-app-tf-state"
+    key            = "tf-state-setup"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "devops-siva-recp-app-tf-lock"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      Environment = terraform.workspace
+      Project     = var.project
+      contact     = var.contact
+      ManageBy    = "Terraform/setup"
+    }
+  }
+}
+
+# Create a VPC
+# resource "aws_vpc" "example" {
+#   cidr_block = "10.0.0.0/16"
+# }
