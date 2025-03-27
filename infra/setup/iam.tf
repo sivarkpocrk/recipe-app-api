ccf2e4c147045f65ae66724f35894a177820d765
sivak1106 <92651216+sivarkpocrk@users.noreply.github.com>
2025-03-27 11:57:57 +0000

    fixed iam policy for rds tagging

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -165,7 +165,9 @@ data "aws_iam_policy_document" "rds" {
       "rds:CreateDBInstance",
       "rds:DeleteDBInstance",
       "rds:ListTagsForResource",
-      "rds:ModifyDBInstance"
+      "rds:ModifyDBInstance",
+      "rds:AddTagsToResource",
+      "rds:ModifyDBSubnetGroup"
     ]
     resources = ["*"]
   }
