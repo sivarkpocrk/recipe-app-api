66d989bac81c146d3e8366f31de2f85447d6c02e
sivak1106 <92651216+sivarkpocrk@users.noreply.github.com>
2025-03-28 12:15:09 +0000

    added load balancer configuration

--- infra/deploy/load_balancer.tf
+++ infra/deploy/load_balancer.tf
@@ -28,3 +28,10 @@ resource "aws_security_group" "lb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_lb" "api" {
+  name               = "${local.prefix}-lb"
+  load_balancer_type = "application"
+  subnets            = [aws_subnet.public_a, aws_subnet.public_b]
+  security_groups    = [aws_security_group.lb.id]
+}
